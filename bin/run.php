d1019b9799cca1dc1e5ea2944a0d8a8d23d43a86
Kostya <kostya.kozma@lehrermarktplatz.de>
2022-08-18 02:20:45 +0300

    Add application

--- bin/run.php
+++ bin/run.php
@@ -5,5 +5,5 @@ use App\Application;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$app = new Application('LMP Comments', '0.0.1');
+$app = new Application('swoole-service', '0.0.1');
 $app->run();
